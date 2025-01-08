d26d930512d4ec175127fa331d2589a4c4ddec9f
JoachimKvamme <kaj082024@gmail.com>
2025-01-08 10:15:45 +0100

    Startet med å lage en egen test som sjekker en primtall-metode

--- Assignment/TestCustom.cs
+++ Assignment/TestCustom.cs
@@ -8,6 +8,23 @@ namespace C_ASSIGNMENT_BUILDER.Assignment
 {
     public class TestCustom : AssignmentBase
     {
-        
+    
+    
+    public bool CheckPrime(int i) {
+
+        bool isPrime = false;
+
+
+        return isPrime;
+    }
+    [Assignment(1)]
+    public void TestPrime()
+    {
+        Assert.Equal(0, Fibonacci(0));
+        Assert.Equal(1, Fibonacci(1));
+        Assert.Equal(1, Fibonacci(2));
+        Assert.Equal(5, Fibonacci(5));
+        Assert.Equal(55, Fibonacci(10));
+    }
     }
 }
