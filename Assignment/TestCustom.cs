64380baed967e3a12398493c96d6cc3f56dcf621
JoachimKvamme <kaj082024@gmail.com>
2025-01-08 10:22:22 +0100

    Laget en metode og en test til den

--- Assignment/TestCustom.cs
+++ Assignment/TestCustom.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using C_ASSIGNMENT_BUILDER.Engine.AssignmentBuilder;
+using Xunit;
 
 namespace C_ASSIGNMENT_BUILDER.Assignment
 {
@@ -10,21 +11,28 @@ namespace C_ASSIGNMENT_BUILDER.Assignment
     {
     
     
-    public bool CheckPrime(int i) {
+    public bool CheckPrime(int n) {
 
-        bool isPrime = false;
+        if (n <= 1) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
 
-
-        return isPrime;
+    var boundary = (int)Math.Floor(Math.Sqrt(n));
+          
+    for (int i = 3; i <= boundary; i += 2)
+        if (n % i == 0)
+            return false;
+    
+    return true;  
     }
     [Assignment(1)]
     public void TestPrime()
     {
-        Assert.Equal(0, Fibonacci(0));
-        Assert.Equal(1, Fibonacci(1));
-        Assert.Equal(1, Fibonacci(2));
-        Assert.Equal(5, Fibonacci(5));
-        Assert.Equal(55, Fibonacci(10));
+        Assert.Equal(true, CheckPrime(2));
+        Assert.Equal(false, CheckPrime(266));
+        Assert.Equal(true, CheckPrime(523));
+        Assert.Equal(true, CheckPrime(5));
+        Assert.Equal(false, CheckPrime(10));
     }
     }
 }
